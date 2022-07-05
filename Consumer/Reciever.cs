1408804a081bfaa1def3eaf81d54874720510a6b
Georyitt Gorayeb <georyitt_gorayeb@hotmail.com>
2022-07-05 15:10:56 -0400

    add integration with RabbitMQ

---
+++ Consumer/Reciever.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "georyitt",
+    Password = "hiphop20"
+};
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare("geoQueue", false, false, false, null);
+    
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.Span;
+        var message = Encoding.UTF8.GetString(body);
+        
+        Console.WriteLine("message receiver: {0}", message);
+    };
+
+    channel.BasicConsume("geoQueue", true, consumer);
+
+}
+
+Console.WriteLine("Press [enter] to exit.");
+Console.ReadLine();
---
+++ Producer/Sender.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "georyitt",
+    Password = "hiphop20"
+};
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare("geoQueue", false, false, false, null);
+    
+    var message = "hello world from geo";
+    var body = Encoding.UTF8.GetBytes(message);
+    
+    channel.BasicPublish("", "geoQueue", null, body);
+    Console.WriteLine("sent {0}", message);
+}
+
+Console.WriteLine("Press [enter] to exit.");
+Console.ReadLine();
+
